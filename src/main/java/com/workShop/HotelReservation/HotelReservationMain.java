9f1604d57e5cad33e4be4dafbacff02cd6467cf3
Parvez Makandar <86365582+parvez050@users.noreply.github.com>
2022-03-09 17:25:44 +0530

    Update HotelReservationMain.java

--- src/main/java/com/workShop/HotelReservation/HotelReservationMain.java
+++ src/main/java/com/workShop/HotelReservation/HotelReservationMain.java
@@ -12,8 +12,8 @@ public class HotelReservationMain {
         hotelReservation.addHotel("Bridgewood", 4, 150,50);
         hotelReservation.addHotel("Ridgewood", 5, 220,150);
         hotelReservation.printHotelList();
-        LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);
-        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);
+        LocalDate startDate = LocalDate.of(2020, Month.SEPTEMBER, 10);
+        LocalDate endDate = LocalDate.of(2020, Month.SEPTEMBER, 11);
         hotelReservation.getCheapestHotel(startDate, endDate);
     }
 }
